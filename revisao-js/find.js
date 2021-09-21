ad997927b3f9dd770b74bb91678968f4cbab58ff
dripivetta <dripivetta@hotmail.com>
2021-09-21 01:16:19 +0000

    (14/09) Criação do projeto React, Links e rotas, useEffect ()

--- revisao-js/find.js
+++ revisao-js/find.js
@@ -1,4 +1,4 @@
-const nums = [10, 19, 3, -4, 13, -11, 15, 0, -1]
+    const nums = [10, 19, 3, -4, 13, -11, 15, 0, -1]
 
 // Encontrar o primeiro número negativo presente no vetor
 const primeiroNeg = nums.find (x => x < 0)
